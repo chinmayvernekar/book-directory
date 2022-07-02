91002ef4f8b50164dd2b5e8ca4465ffc6d3bae68
chinmayvernekar <chinmay.vernekar49@gmail.com>
2022-07-02 22:14:31 +0530

    initial commit

--- src/main/java/com/example/bookdirectory/SecurityConfig.java
+++ src/main/java/com/example/bookdirectory/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/register-user","/save-role",
                 "/authenticate","/swagger/**",
